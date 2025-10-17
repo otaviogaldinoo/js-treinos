82ac8d963da77cd8e711d0b020ce2b0b24e97851
otavio <suaresotavio@gmail.com>
2025-10-17 16:56:29 -0300

    revisar operadores

---
+++ dia02-operadores/operadores.js
@@ -0,0 +1,40 @@
+// tipos de numeros
+let idade = 20; // número inteiro
+let altura = 1.75; // número float
+console.log(typeof idade); // resultado: number
+console.log(typeof altura); // resultado: number
+
+// operadores aritméticos
+let a = 10;
+let b = 3;
+console.log(a + b); // adição: resultado 13
+console.log(a - b); // subtração: resultado 7
+console.log(a * b); // multiplicação: resultado 30
+console.log(a / b); // divisão: resultado 3.3333...
+console.log(a % b); // módulo: resultado 1
+
+
+// operadores de incremento e decremento
+let contador = 5;
+contador++; // incremento
+console.log(contador); // resultado: 6
+contador--; // decremento
+console.log(contador); // resultado: 5
+
+// operadores de atribuição
+let x = 10;
+x += 5; // equivalente a x = x + 5
+console.log(x); // resultado: 15
+x *= 2; // equivalente a x = x * 2
+console.log(x); // resultado: 30
+
+// operadores de comparação
+let idade1 = 18;
+let idade2 = 21;
+console.log(idade1 == idade2); // igual a: resultado false
+console.log(idade1 != idade2); // diferente de: resultado true
+console.log(idade1 < idade2); // menor que: resultado true
+console.log(idade1 > idade2); // maior que: resultado false
+console.log(idade1 <= idade2); // menor ou igual a: resultado true
+console.log(idade1 >= idade2); // maior ou igual a: resultado false
+
